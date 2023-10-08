a4f0c7014678e22446ab4ef2e63595b14da4bc2d
bhaskarkhan <147291174+bhaskarkhan@users.noreply.github.com>
2023-10-08 17:14:02 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <Provider store={store}>
+    <App />
+    </Provider> 
+   
+  </BrowserRouter>,
+);
+
+reportWebVitals();
